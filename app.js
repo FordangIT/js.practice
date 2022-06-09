27b3ce7a91430aa71b85c172d9ff737b5f37e8f7
FordangIT <93567754+FordangIT@users.noreply.github.com>
2022-06-09 19:42:53 +0900

    true false null undefined

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const num1 = 2;
-const num2 = 5;
-const myName = "망뚜";
+const amIFat = null; 
+console.log(amIFat); 
+//로그인했는가? //null
+//variable 안에 어떤 것이 없다는 것을 확실히 하기 위해 쓰는 것 
+//자바스크립트에게 여긴 값이 없다는 것을 알려주는 것 
+let something; 
+console.log(something);
+//undefined; 
+// variale은 주어져있는데 값이 없는 것 
 
-console.log(num1 + num2);
-console.log(num1 * num2);
-console.log(num1 / num2);
-console.log("Hello " + myName);
-
-myName= "유댕";
-console.log("Hello " + myName); 
