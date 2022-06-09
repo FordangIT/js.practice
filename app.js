170c52730edaf366becd1802234116d049e460f1
FordangIT <93567754+FordangIT@users.noreply.github.com>
2022-06-09 20:16:34 +0900

    array=[ ]

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const amIFat = null; 
-console.log(amIFat); 
-//로그인했는가? //null
-//variable 안에 어떤 것이 없다는 것을 확실히 하기 위해 쓰는 것 
-//자바스크립트에게 여긴 값이 없다는 것을 알려주는 것 
-let something; 
-console.log(something);
-//undefined; 
-// variale은 주어져있는데 값이 없는 것 
+/*const mon = "mon"
+const tue = "tue"
+const wed = "wed"
+const thu = "thu"
+const fri = "fri"
+const sat = "sat"
+const sun = "sun"
+const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun,"sun"]; 
+const nonsense = [1, 2, "Hello", false, null, true, undefined];
+console.log(daysOfWeek, nonsense);
+*/
 
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+//get item from array
+console.log(daysOfWeek[4]);
+
+//add one more day to the array
+daysOfWeek.push("sun"); 
+console.log(daysOfWeek);
+
+//array의 목적은, 하나의 variable 안에 데이터의 list 값을 가지는 것
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("고래밥");
+console.log(toBuy); 
