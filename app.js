c062bee53e2e3801949cab36046daf55ffe12377
FordangIT <93567754+FordangIT@users.noreply.github.com>
2022-06-09 11:28:41 +0900

    js #2..0#2.1#2.2#2.3
    integar, string, const, let, console.log()

---
+++ app.js
@@ -0,0 +1,11 @@
+const num1 = 2;
+const num2 = 5;
+const myName = "망뚜";
+
+console.log(num1 + num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log("Hello " + myName);
+
+myName= "유댕";
+console.log("Hello " + myName); 
